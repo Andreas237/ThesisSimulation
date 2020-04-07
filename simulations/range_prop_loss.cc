100b3695ae4ef94ffbf1b83c85e26cbe03dbcc57
Andreas237 <silenta237@gmail.com>
2020-04-07 17:46:13 -0600

    Nodes are moving!  Mobility Model in place, though only 2D movement

--- simulations/range_prop_loss.cc
+++ simulations/range_prop_loss.cc
@@ -1,10 +1,9 @@
 #include "ns3/core-module.h"
-#include "ns3/network-module.h"
-#include "ns3/internet-module.h"
-#include "ns3/point-to-point-module.h"
-#include "ns3/applications-module.h"
+#include "ns3/mobility-module.h"
 #include "ns3/propagation-loss-model.h"
 #include "ns3/spectrum-channel.h"
+#include "ns3/spectrum-helper.h"
+#include "ns3/vector.h"
 
 
 
@@ -15,8 +14,8 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("MyRangedPropogationLossModel");
 
-void
-LogNodeContainer(std::string, NodeContainer);
+void CourseChange (std::string, Ptr<const MobilityModel>);
+void LogNodeContainer(std::string, NodeContainer);
 
 
 
@@ -33,33 +32,111 @@ main (int argc, char *argv[])
 
     // Update defaults per command line
     CommandLine cmd;
-    cmd.AddValue("nJammers","Number of jamming nodes",nJammer);
+    cmd.AddValue("nJammer","Number of jamming nodes",nJammer);
     cmd.AddValue("nBsr","Number of BSR nodes",nBsr);
     cmd.Parse (argc,argv);
 
+
+
+    /*
+        Don't allow more than 5 jammers otherwise the field will be too crowded
+        with interfering signals.
+        Don't allow more than 5 BSRs just because...
+    */
+    if (nJammer > 5 )
+    {
+        std::cout << "nJammer should be 5 or less; otherwise grid layout exceeds the bounding box" << std::endl;
+        return 1;
+    }
+    if (nBsr > 5 )
+    {
+        std::cout << "nBsr should be 5 or less; otherwise grid layout exceeds the bounding box" << std::endl;
+        return 1;
+    }
+
+
+
+
+
+
     // Create jamming node
     // Add transmitter node to receiver network
-    NodeContainer jammer;
-    jammer.Create(nJammer);
+    NodeContainer jammerNodes;
+    jammerNodes.Create(nJammer);
 
 
     // Create Receiver Nodes
     NodeContainer bsr;
     bsr.Create(nBsr);
 
-    // Set channel attributes
-    // data rate
-    // delay
-    // Create station node (Tut 3, for WiFi)
-
-
-    // Construct PHY and channel helpers
+    // TODO: Construct PHY and channel helpers
     //      -- What are appropriate helpers?
     //      -- What makes sense for my model?
+    //      -- PathLoss Model here?
+    SpectrumChannelHelper channel = SpectrumChannelHelper::Default();
 
 
 
-    LogNodeContainer("jammer",jammer);
+    // TODO: Create mobility model
+    //      -- Jammer moves in spherical coordinates
+    //      -- BSRs are fixed
+    //      -- Log movement of Jammers
+    //      -- TODO: probably could use SetMobilityModel=RandomWaypoint
+    MobilityHelper mobility;            // mobility model
+    mobility.SetPositionAllocator("ns3::RandomDiscPositionAllocator",
+                                  "X",DoubleValue(0.0),
+                                  "Y",DoubleValue(0.0),
+                                  "Z",DoubleValue(0.0),
+                                  "Rho", StringValue ("ns3::UniformRandomVariable[Min=1|Max=2]"),       // don't want them taking up too much space
+                                  "Theta", StringValue ("ns3::UniformRandomVariable[Min=0|Max=40]")     // 40 unit radius?
+                                  );
+    // Set the mobility model type
+    mobility.SetMobilityModel ("ns3::RandomWalk2dMobilityModel",
+                            "Bounds", RectangleValue (Rectangle (-50, 50, -50, 50)));
+    // install the mobility model to some nodes
+    mobility.Install(jammerNodes);
+
+
+
+
+    // Set the BSR to be fixed...
+    //TODO: If multiple BSRs position them differently
+    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+    mobility.Install (bsr);
+
+
+
+
+
+    // log the movement of the nodes!  (Same as with third.cc)
+    std::ostringstream oss;
+    
+    oss <<
+    "/NodeList/" << jammerNodes.Get (nJammer - 1)->GetId () <<
+    "/$ns3::MobilityModel/CourseChange";
+
+    /*
+    for(int i=1; i <= nJammer; i++){
+        oss <<
+        "/NodeList/" << jammerNodes.Get (nJammer - i)->GetId () <<
+        "/$ns3::MobilityModel/CourseChange";
+    }
+    */
+
+    Config::Connect (oss.str (), MakeCallback(&CourseChange));
+
+
+
+
+
+    // Simulator stop
+    Simulator::Stop (Seconds (10.0));
+
+
+
+
+    // Call my logger functions to test cmd input
+    LogNodeContainer("jammerNodes",jammerNodes);
     LogNodeContainer("bsr",bsr);
 
 
@@ -77,6 +154,14 @@ main (int argc, char *argv[])
 
 
 
+
+
+
+
+
+
+
+
 void
 LogNodeContainer(std::string ContainerName, NodeContainer container) {
     std::ostringstream oss;
@@ -85,3 +170,25 @@ LogNodeContainer(std::string ContainerName, NodeContainer container) {
     "\tContainer this many nodes=" << container.GetN();
     NS_LOG_UNCOND(oss.str());
 }// end LogNodeContainer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void
+CourseChange (std::string context, Ptr<const MobilityModel> model)
+{
+  Vector position = model->GetPosition ();
+  NS_LOG_UNCOND (context <<
+    " x = " << position.x << ", y = " << position.y << ", z = " << position.z);
+}
